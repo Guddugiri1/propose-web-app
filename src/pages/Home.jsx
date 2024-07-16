56eb79f42d9d8a14fe76572ad51e5ff58c4bb12f
Guddu Giri <guddugiri216@gmail.com>
2024-07-16 13:10:25 +0530

    Your commit message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,12 +20,8 @@ const Home = ({ className = "" }) => (
             <ProposeForm className="hero_text-form mt-4 mt-md-5" />
             <p className="hero_credit">
               developed by{" "}
-              <a
-                href="https://urlwebwala.in"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                @URLWEBWALA
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                @guddu Giri
               </a>
             </p>
           </div>
